1d88d42834ebc7bcd8d097ef7d8a8225f932b9cf
Andrei Ignat <ignatandrei@yahoo.com>
2024-07-29 09:01:48 +0300

    v2024.729.2101

--- src/RSCG_ExportDiagram/RSCG_ExportDiagram/ExportClass.cs
+++ src/RSCG_ExportDiagram/RSCG_ExportDiagram/ExportClass.cs
@@ -18,52 +18,65 @@ public class ExportAssembly
             .SelectMany(it => it.MethodsWithExternalReferences)
             .SelectMany(it => it.References)
             .GroupBy(it => it.AssemblyName)
-            .ToDictionary(it=>it.Key,it=>it.ToArray());
-        
-        
-       var sb = new StringBuilder();
+            .ToDictionary(it => it.Key, it => it.ToArray());
+
+
+        var sb = new StringBuilder();
         sb.AppendLine($"# {AssemblyName}");
         sb.AppendLine();
         sb.AppendLine("```mermaid");
         sb.AppendLine("flowchart TD");//TB?
         sb.AppendLine($"subgraph {AssemblyName}");
-
+        sb.AppendLine($"style {AssemblyName} fill:#f9f,stroke:#333,stroke-width:4px");
 
         foreach (var expClass in this.ClassesWithExternalReferences)
         {
             //main assembly
             sb.AppendLine($"subgraph {expClass.ClassName}");
-            foreach(var met in expClass.MethodsWithExternalReferences)
+            foreach (var met in expClass.MethodsWithExternalReferences)
             {
-                sb.AppendLine($"{met.MethodName}");
+                //TODO: add full method name
+                sb.AppendLine($"{expClass.ClassName}.{met.MethodName}[{met.MethodName}]");
             }
             sb.AppendLine($"%% end of subgraph {expClass.ClassName}");
             sb.AppendLine("end");
-
-            foreach (var item in assemblyReferences.Keys)
-            {
-                sb.AppendLine($"subgraph {item}");
-
-                var methods = assemblyReferences[item]
-                    .Select(it => it.FullName)                    
-                    .Distinct()
-                    .ToArray();
-                foreach (var met in methods)
-                {
-                    var lastDot = met.LastIndexOf('.');
-                    var nameMethod = met.Substring(lastDot+ 1);  
-                    sb.AppendLine($"{met}[{nameMethod}]");
-                }
-
-                sb.AppendLine($"%% end of subgraph {expClass.ClassName}");
-                sb.AppendLine("end");
-
-            }
-
-
         }
         sb.AppendLine($"%% end of subgraph {AssemblyName}");
         sb.AppendLine($"end");
+
+        foreach (var item in assemblyReferences.Keys)
+        {
+            sb.AppendLine($"subgraph {item}");
+
+            var methods = assemblyReferences[item]
+                .Select(it => it.FullName)
+                .Distinct()
+                .ToArray();
+            foreach (var met in methods)
+            {
+                var lastDot = met.LastIndexOf('.');
+                lastDot = met.LastIndexOf('.', lastDot - 1);
+                var nameMethod = met.Substring(lastDot + 1);
+                sb.AppendLine($"{met}[{nameMethod}]");
+            }
+
+            sb.AppendLine($"%% end of subgraph {item}");
+            sb.AppendLine($"end");
+
+        }
+
+
+        foreach (var item in this.ClassesWithExternalReferences)
+        {
+            foreach (var met in item.MethodsWithExternalReferences)
+            {
+                string nameMethod = met.MethodName; 
+                foreach (var refItem in met.References)
+                {
+                    sb.AppendLine($"{item.ClassName}.{nameMethod} --> {refItem.FullName}");
+                }
+            }
+        }
         sb.AppendLine("```");
 
         return sb.ToString();
