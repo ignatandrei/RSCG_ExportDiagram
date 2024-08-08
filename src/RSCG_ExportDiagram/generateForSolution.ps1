5b4f6d9a9cb95ce49e813dfb6b5a51225ce3bd11
Andrei Ignat <ignatandrei@yahoo.com>
2024-08-08 20:46:18 +0300

    adding export for sln

--- src/RSCG_ExportDiagram/generateForSolution.ps1
+++ src/RSCG_ExportDiagram/generateForSolution.ps1
@@ -1,33 +1,3 @@
-# RSCG_ExportDiagram
-export diagram for external relations for a csproj   
-
-## Install
-
-Add to the csproj
-    
-```xml
-
-<ItemGroup>
-<PackageReference Include="RSCG_ExportDiagram" Version="2024.806.2104" OutputItemType="Analyzer" ReferenceOutputAssembly="false"   />
-</ItemGroup>
-<ItemGroup>
-	<CompilerVisibleProperty Include="RSCG_ExportDiagram_OutputFolder" />
-	<CompilerVisibleProperty Include="RSCG_ExportDiagram_Exclude" />
-</ItemGroup>	
-<PropertyGroup>
-<RSCG_ExportDiagram_OutputFolder>..</RSCG_ExportDiagram_OutputFolder>
-<RSCG_ExportDiagram_Exclude></RSCG_ExportDiagram_Exclude>
-</PropertyGroup>
-
-```
-
-
-    And the diagram will be generated in the folder parent for the .csproj file
-
-Alternatively, you can use the command line tool to generate the diagram for a solution
-
-```powershell
-
 function ProcessCsproj {
   param (
       [string]$project,
@@ -95,9 +65,4 @@ Get-Content $solution |
       Write-Host $fileProject
       ProcessCsproj -project $fileProject -folderOutput $folderSolution
     }
-
-
-
-```
-    
 
